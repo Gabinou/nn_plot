c5c2c257890a70a1512113e2e17339bddf8f1c72
Gabriel Taillon <ga.taillon@gmail.com>
2018-11-18 22:21:35 +0900

    Revises codestyle, gets rids of some useless lines, add the title keyword arguement to the neuralNetwork.draw method.

--- nnplot.py
+++ nnplot.py
@@ -18,26 +18,27 @@ class Neuron():
 
 
 class Layer():
-    def __init__(self, network, neuron_num, neuron_num_widest, neuron_radius, line_weights, line_colors, neuron_color, neuron_text):
-        
-        self.layer_distance = 6
-        self.distance_neurons = 2
+    def __init__(self, network, neuron_num, neuron_num_widest, neuron_radius, line_weights, line_colors, neuron_color, neuron_text, distance_layers=6, distance_neurons=2):
+
+        self.distance_layers = distance_layers
+        self.distance_neurons = distance_neurons
         self.neuron_radius = neuron_radius
         self.neuron_text = neuron_text
         self.neuron_num_widest = neuron_num_widest
         self.previous_layer = self.__get_previous_layer(network)
         self.direction = network.direction
-        if (self.direction == 'bottomtotop') or (self.direction == 'toptobottom'):  
+        if (self.direction == 'bottomtotop') or (self.direction == 'toptobottom'):
             self.y = self.__layer_position()
         elif (self.direction == 'righttoleft') or (self.direction == 'lefttoright'):
             self.x = self.__layer_position()
-        self.neurons = self.__intialise_neurons(neuron_num, neuron_color, neuron_text)
+        self.neurons = self.__intialise_neurons(
+            neuron_num, neuron_color, neuron_text)
         self.line_weights = line_weights
         self.line_colors = line_colors
 
     def __intialise_neurons(self, neuron_num, neuron_color='k', neuron_text=''):
         neurons = []
-        if (self.direction == 'bottomtotop') or (self.direction == 'toptobottom'):  
+        if (self.direction == 'bottomtotop') or (self.direction == 'toptobottom'):
             self.x = self.__lmargin_for_centering(neuron_num)
         elif (self.direction == 'righttoleft') or (self.direction == 'lefttoright'):
             self.y = self.__lmargin_for_centering(neuron_num)
@@ -52,7 +53,7 @@ class Layer():
                 text = neuron_text[iteration]
             neuron = Neuron(self.x, self.y, color, text)
             neurons.append(neuron)
-            if (self.direction == 'bottomtotop') or (self.direction == 'toptobottom'): 
+            if (self.direction == 'bottomtotop') or (self.direction == 'toptobottom'):
                 self.x += self.distance_neurons
             elif (self.direction == 'righttoleft') or (self.direction == 'lefttoright'):
                 self.y += self.distance_neurons
@@ -64,13 +65,13 @@ class Layer():
     def __layer_position(self):
         if self.previous_layer:
             if (self.direction == 'bottomtotop'):
-                return self.previous_layer.y + self.layer_distance
-            elif (self.direction == 'toptobottom'): 
-                return self.previous_layer.y - self.layer_distance
-            elif (self.direction == 'lefttoright'): 
-                return self.previous_layer.x + self.layer_distance
-            elif (self.direction == 'righttoleft'):   
-                return self.previous_layer.x - self.layer_distance  
+                return self.previous_layer.y + self.distance_layers
+            elif (self.direction == 'toptobottom'):
+                return self.previous_layer.y - self.distance_layers
+            elif (self.direction == 'lefttoright'):
+                return self.previous_layer.x + self.distance_layers
+            elif (self.direction == 'righttoleft'):
+                return self.previous_layer.x - self.distance_layers
         else:
             return 0
 
@@ -81,7 +82,7 @@ class Layer():
             return None
 
     def __line_two_neurons(self, neuron1, neuron2, line_weight=1, linecolor='k'):
-        if (self.direction == 'bottomtotop') or (self.direction == 'toptobottom'): 
+        if (self.direction == 'bottomtotop') or (self.direction == 'toptobottom'):
             angle = np.arctan((neuron2.x - neuron1.x) /
                               float(neuron2.y - neuron1.y))
             x_adjustment = self.neuron_radius * np.sin(angle)
@@ -89,7 +90,7 @@ class Layer():
         elif (self.direction == 'righttoleft') or (self.direction == 'lefttoright'):
             angle = np.arctan((neuron2.y - neuron1.y) /
                               float(neuron2.x - neuron1.x))
-            x_adjustment = self.neuron_radius * np.cos(angle) 
+            x_adjustment = self.neuron_radius * np.cos(angle)
             y_adjustment = self.neuron_radius * np.sin(angle)
         if (self.direction == 'bottomtotop'):
             line_x1 = neuron1.x - x_adjustment
@@ -112,7 +113,7 @@ class Layer():
             line_y1 = neuron1.y + y_adjustment
             line_y2 = neuron2.y - y_adjustment
         line = plt.Line2D((line_x1, line_x2),
-                          (line_y1, line_y2), 
+                          (line_y1, line_y2),
                           alpha=line_weight, color=linecolor)
         plt.gca().add_line(line)
 
@@ -127,7 +128,7 @@ class Layer():
                         line_weight = self.previous_layer.line_weights
                     else:
                         line_weight = self.previous_layer.line_weights[this_layer_neuron_index,
-                                                             previous_layer_neuron_index]
+                                                                       previous_layer_neuron_index]
                     if isinstance(self.previous_layer.line_colors, str):
                         linecolor = self.previous_layer.line_colors
                     else:
@@ -136,7 +137,7 @@ class Layer():
                     self.__line_two_neurons(
                         neuron, previous_layer_neuron, line_weight, linecolor)
         # write Text
-        if (self.direction == 'bottomtotop') or (self.direction == 'toptobottom'): 
+        if (self.direction == 'bottomtotop') or (self.direction == 'toptobottom'):
             x_text = self.neuron_num_widest * self.distance_neurons
             if layerType == 0:
                 plt.text(x_text, self.y, 'Input Layer', fontsize=12)
@@ -156,6 +157,7 @@ class Layer():
                 plt.text(x_text, y_text, 'Hidden Layer ' +
                          str(layerType), fontsize=12)
 
+
 class NeuralNetwork():
     def __init__(self, neuron_num_widest, neuron_radius=0.5, direction='lefttoright'):
         self.neuron_num_widest = neuron_num_widest
@@ -163,13 +165,13 @@ class NeuralNetwork():
         self.layertype = 0
         self.direction = direction
         self.neuron_radius = neuron_radius
- 
+
     def add_layer(self, neuron_num, neuron_radius=0.5, line_weights=1, line_colors='k', neuron_color='k', neuron_text=''):
         layer = Layer(self, neuron_num, self.neuron_num_widest, neuron_radius,
                       line_weights, line_colors, neuron_color, neuron_text)
         self.layers.append(layer)
 
-    def draw(self):
+    def draw(self, title=''):
         plt.figure(figsize=(10, 6))
         for i in range(len(self.layers)):
             layer = self.layers[i]
@@ -178,23 +180,21 @@ class NeuralNetwork():
             layer.draw(i)
         plt.axis('scaled')
         plt.axis('off')
-        # plt.title( 'Neural Network architecture', fontsize=15 )
+        plt.title(title, fontsize=15)
         plt.gcf().tight_layout()
-        # transform = Affine2D().rotate_deg(270)
-        # plot_extents = 0, 10, 0, 10
-        # helper = floating_axes.GridHelperCurveLinear(transform, plot_extents)
         plt.show()
 
 
 network = NeuralNetwork(10)
 # line_weights to convert from 10 outputs to 4 (decimal digits to their binary representation)
 line_weights1 = np.array([[0, 0, 0, 0, 1, 0.3, 0, 0, 1, 1],
-                     [0, 0, 0, 0, 1, 1, 1, 1, 0, 0],
-                     [0, 0, 1, 1, 0, 0, 1, 1, 0, 0],
-                     [0, 1, 0, 1, 0, 1, 0, 1, 0, 1]])
+                          [0, 0, 0, 0, 1, 1, 1, 1, 0, 0],
+                          [0, 0, 1, 1, 0, 0, 1, 1, 0, 0],
+                          [0, 1, 0, 1, 0, 1, 0, 1, 0, 1]])
 neuron_text1 = np.array(['0.1', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'])
 print(line_weights1.shape)
-network.add_layer(10, line_weights=line_weights1, line_colors='b', neuron_text=neuron_text1)
+network.add_layer(10, line_weights=line_weights1,
+                  line_colors='b', neuron_text=neuron_text1)
 network.add_layer(4, neuron_color='b', )
 network.add_layer(1)
-network.draw()
+network.draw(, title='')
